96bb8937cf8082b76129c81dcba39002c176d6ca
student <student@3-6>
2025-01-28 20:23:50 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.UrlLabel = new System.Windows.Forms.Label();
+            this.savePathBox = new System.Windows.Forms.TextBox();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadsList = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 15;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.Controls.Add(this.UrlLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.savePathBox, 11, 1);
+            this.tableLayoutPanel1.Controls.Add(this.urlBox, 14, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 11, 0);
+            this.tableLayoutPanel1.Controls.Add(this.downloadsList, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.progressBar, 3, 13);
+            this.tableLayoutPanel1.Controls.Add(this.startButton, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pauseButton, 5, 4);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 5, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 8);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 15;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // UrlLabel
+            // 
+            this.UrlLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.UrlLabel, 4);
+            this.UrlLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.UrlLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UrlLabel.Location = new System.Drawing.Point(3, 0);
+            this.UrlLabel.Name = "UrlLabel";
+            this.UrlLabel.Size = new System.Drawing.Size(206, 30);
+            this.UrlLabel.TabIndex = 0;
+            this.UrlLabel.Text = "Введите Url:";
+            this.UrlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // savePathBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.savePathBox, 4);
+            this.savePathBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.savePathBox.Location = new System.Drawing.Point(586, 33);
+            this.savePathBox.Name = "savePathBox";
+            this.savePathBox.Size = new System.Drawing.Size(211, 23);
+            this.savePathBox.TabIndex = 1;
+            this.savePathBox.DoubleClick += new System.EventHandler(this.browseButton_Click);
+            // 
+            // urlBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.urlBox, 4);
+            this.urlBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.urlBox.Location = new System.Drawing.Point(3, 33);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(206, 23);
+            this.urlBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 4);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(586, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите путь сохранения:\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // downloadsList
+            // 
+            this.downloadsList.AllowDrop = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.downloadsList, 3);
+            this.downloadsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.downloadsList.FormattingEnabled = true;
+            this.downloadsList.HorizontalScrollbar = true;
+            this.downloadsList.ItemHeight = 15;
+            this.downloadsList.Location = new System.Drawing.Point(3, 273);
+            this.downloadsList.Name = "downloadsList";
+            this.tableLayoutPanel1.SetRowSpan(this.downloadsList, 6);
+            this.downloadsList.ScrollAlwaysVisible = true;
+            this.downloadsList.Size = new System.Drawing.Size(153, 174);
+            this.downloadsList.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.progressBar, 12);
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.Location = new System.Drawing.Point(162, 393);
+            this.progressBar.Name = "progressBar";
+            this.tableLayoutPanel1.SetRowSpan(this.progressBar, 2);
+            this.progressBar.Size = new System.Drawing.Size(635, 54);
+            this.progressBar.TabIndex = 5;
+            // 
+            // startButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.startButton, 5);
+            this.startButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.startButton.Location = new System.Drawing.Point(268, 33);
+            this.startButton.Name = "startButton";
+            this.tableLayoutPanel1.SetRowSpan(this.startButton, 2);
+            this.startButton.Size = new System.Drawing.Size(259, 54);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Начать загрузку";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pauseButton, 5);
+            this.pauseButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pauseButton.Location = new System.Drawing.Point(268, 123);
+            this.pauseButton.Name = "pauseButton";
+            this.tableLayoutPanel1.SetRowSpan(this.pauseButton, 2);
+            this.pauseButton.Size = new System.Drawing.Size(259, 54);
+            this.pauseButton.TabIndex = 7;
+            this.pauseButton.Text = "Приостановить загрузку";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.cancelButton, 5);
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cancelButton.Location = new System.Drawing.Point(268, 213);
+            this.cancelButton.Name = "cancelButton";
+            this.tableLayoutPanel1.SetRowSpan(this.cancelButton, 2);
+            this.cancelButton.Size = new System.Drawing.Size(259, 54);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Отмена загрузки";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 3);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Список загрузок:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label UrlLabel;
+        private TextBox savePathBox;
+        private TextBox urlBox;
+        private Label label1;
+        private ListBox downloadsList;
+        private ProgressBar progressBar;
+        private Button startButton;
+        private Button pauseButton;
+        private Button cancelButton;
+        private Label label2;
+    }
+}
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private WebClient _webClient;
+        private CancellationTokenSource _cancellationTokenSource;
+        private string _downloadFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog saveFileDialog = new FolderBrowserDialog())
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    savePathBox.Text = saveFileDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(urlBox.Text) || string.IsNullOrEmpty(savePathBox.Text))
+            {
+                MessageBox.Show("Введите URL и путь для сохранения.");
+                return;
+            }
+
+            _downloadFilePath = savePathBox.Text;
+
+            _webClient = new WebClient();
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            _webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+            _webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+
+            try
+            {
+                Uri downloadUri = new Uri(urlBox.Text);
+                _webClient.DownloadFileAsync(downloadUri, _downloadFilePath);
+                downloadsList.Items.Add($"Загрузка начата: {urlBox.Text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void WebClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                downloadsList.Items.Add($"Загрузка отменена: {_downloadFilePath}");
+            }
+            else if (e.Error != null)
+            {
+                downloadsList.Items.Add($"Ошибка загрузки: {_downloadFilePath} - {e.Error.Message}");
+            }
+            else
+            {
+                downloadsList.Items.Add($"Загрузка завершена: {_downloadFilePath}");
+            }
+
+            progressBar.Value = 0;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (_webClient != null)
+            {
+                _webClient.CancelAsync();
+                downloadsList.Items.Add($"Загрузка приостановлена: {_downloadFilePath}");
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (_webClient != null)
+            {
+                _webClient.CancelAsync();
+                downloadsList.Items.Add($"Загрузка отменена: {_downloadFilePath}");
+                progressBar.Value = 0;
+
+                if (File.Exists(_downloadFilePath))
+                {
+                    File.Delete(_downloadFilePath);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
